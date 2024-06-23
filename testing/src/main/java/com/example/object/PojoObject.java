bea4b4bfaf76bde14cbc623e49309a531a0a81f1
maaarcsilva3 <maaarc.silva3@gmail.com>
2024-06-23 23:47:32 +0800

    java web service template

---
+++ testing/src/main/java/com/example/object/PojoObject.java
@@ -0,0 +1,56 @@
+package com.example.object;
+
+public class PojoObject {
+	private String longName;
+	private String shortName;
+	private String viscosity;
+	private String packing;
+	private String boxContents;
+	private String wholesalerPrice;
+	private String srp;
+	
+	public String getLongName() {
+		return longName;
+	}
+	public void setLongName(String longName) {
+		this.longName = longName;
+	}
+	public String getShortName() {
+		return shortName;
+	}
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+	public String getViscosity() {
+		return viscosity;
+	}
+	public void setViscosity(String viscosity) {
+		this.viscosity = viscosity;
+	}
+	public String getPacking() {
+		return packing;
+	}
+	public void setPacking(String packing) {
+		this.packing = packing;
+	}
+	public String getBoxContents() {
+		return boxContents;
+	}
+	public void setBoxContents(String boxContents) {
+		this.boxContents = boxContents;
+	}
+	public String getWholesalerPrice() {
+		return wholesalerPrice;
+	}
+	public void setWholesalerPrice(String wholesalerPrice) {
+		this.wholesalerPrice = wholesalerPrice;
+	}
+	public String getSrp() {
+		return srp;
+	}
+	public void setSrp(String srp) {
+		this.srp = srp;
+	}
+	
+	
+}
--- testing/src/main/java/com/example/rest/HelloWorldResource.java
+++ testing/src/main/java/com/example/rest/HelloWorldResource.java
@@ -1,9 +1,15 @@
 package com.example.rest;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.example.object.PojoObject;
+import com.google.gson.Gson;
 
 @Path("/hello")
 public class HelloWorldResource {
@@ -21,5 +27,47 @@ public class HelloWorldResource {
     public String testPath() {
         return "Test path is working";
     }
+    
+    @GET
+    @Path("/getproduct")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getJson() {
+    	
+    	ArrayList <PojoObject> list = new ArrayList();
+    	
+    	PojoObject pojo = new PojoObject();
+    	pojo.setLongName("Repsol Leader Super (Gas and Diesel) Semi Synthetic");
+    	pojo.setShortName("Repsol Leader Super Semi Synthetic");
+    	pojo.setViscosity("20W50 API SL/CF");
+    	pojo.setPacking("1L");
+    	pojo.setBoxContents("12X1L");
+    	pojo.setWholesalerPrice("245");
+    	pojo.setSrp("300/Liter");
+    	
+    	list.add(pojo);
+    	
+    	PojoObject pojo1 = new PojoObject();
+    	pojo1.setLongName("Repsol Leader Super (Gas and Diesel) Semi Synthetic");
+    	pojo1.setShortName("Repsol Leader Super Semi Synthetic");
+    	pojo1.setViscosity("20W50 API SL/CF");
+    	pojo1.setPacking("4L");
+    	pojo1.setBoxContents("5X4L");
+    	pojo1.setWholesalerPrice("945");
+    	pojo1.setSrp("300/Liter");
+    	
+    	list.add(pojo1);
+    	
+    	   Gson gson = new Gson();
+           
+           // Convert ArrayList<PojoObject> to JSON string
+           String json = gson.toJson(list);
+            
+        	return Response.ok(json)
+        			.header("Access-Control-Allow-Origin", "*")
+        			.header("Access-Control-Allow-Methods", "GET")
+        			.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+        			.build();
+    }
+    
       
 }
